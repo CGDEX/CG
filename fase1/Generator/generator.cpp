d113444fd429984b8e00ce473ac58067b5180452
Nuno Silva <nuno200296@gmail.com>
2019-03-02 00:45:18 +0000

    criacao da box

--- fase1/Generator/generator.cpp
+++ fase1/Generator/generator.cpp
@@ -12,8 +12,8 @@ std::vector<vertices> dados;
 
 
 // FUNÇÕES
-std::vector<vertices> esfera (float raio, int camadasX, int camadasY);
-std::vector<vertices> cone (float raio, float altura,  int camadasX, int camadasY);
+std::vector<vertices> esfera (float raio, int layersX, int layersY);
+std::vector<vertices> cone (float raio, float altura,  int layersX, int layersY);
 void escreverFicheiro(std::string, std::vector<vertices> dados);
 
 
@@ -67,15 +67,15 @@ void escreverFicheiro(std::string path, std::vector<vertices> coordenadas) {
 
 
 
-std::vector<vertices> esfera (float raio, int camadasH, int camadasV) {
+std::vector<vertices> esfera (float raio, int layersH, int layersV) {
 
-    // STACKS - CAMADAS HORIZONTAIS
+    // STACKS - layers HORIZONTAIS
     // SLICES - VERTICAL
 
     std::vector<vertices> resultado;
 
-    float deslV = (2*M_PI)/camadasH;
-    float deslH = (M_PI)/camadasV;
+    float deslV = (2*M_PI)/layersH;
+    float deslH = (M_PI)/layersV;
 
     vertices coordenadas1;
     vertices coordenadas2;
@@ -84,9 +84,9 @@ std::vector<vertices> esfera (float raio, int camadasH, int camadasV) {
 
     float a,b = 0;
 
-    for (int i=0;i<camadasH;i++) {
+    for (int i=0;i<layersH;i++) {
         a=0;
-        for(int j=0;j<camadasV;j++) {
+        for(int j=0;j<layersV;j++) {
 
 
             coordenadas1.x = raio*sin(b)*sin(a);
@@ -208,80 +208,274 @@ std::vector<vertices> cone (float raio, float altura,  int slices, int stacks) {
 
 std::vector<vertices> box(float width, float height, float length, float layers) {
 
-    std::vector<vertices> resultado;
+	std::vector<vertices> resultado;
+    
+	vertices coordenadas1;
+	vertices coordenadas2;
+	vertices coordenadas3;
+	vertices coordenadas4;
+	vertices coordenadas5;
+	vertices coordenadas6;
 
-    vertices coordenadas1;
-    vertices coordenadas2;
-    vertices coordenadas3;
-    vertices coordenadas4;
-    vertices coordenadas5;
-    vertices coordenadas6;
+	float x, y, z, xx, yy, zz;
+	float pontoX, pontoY, pontoZ, r, s;
 
-    float x_pos, x_shift, y_pos, y_shift, z_pos, z_shift;
+	//definição dos espaços entre as layers
+	float espC = length / layers;
+	float espL = width / layers;
+	float espA = height / layers;
 
-    y_pos = height / 2;
-    if (layers == 0) {
-        y_shift = height;
-    }
-    else {
-        y_shift = height / layers;
-    }
+	//fazer as faces da frente e face de trás.
+	y = height / 2;
+	yy = -y;
+	x = length / 2;
+	xx = -x;
+	z = length / 2;
+	zz = -z;
 
-    x_pos = width / 2;
-    if (layers == 0) {
-        x_shift = width;
-        layers = 1;
-    }
-    else {
-        x_shift = width / layers;
-    }
+	//faz as layers percorrendo a linha X e quando acabar sobe uma posiçao de Y.
+	for (int i = 0; i < layers; i++) {
+		s = yy + (espA * i);
 
-    z_pos = length / 4;
-    if (layers == 0) {
-        z_shift = length;
-        layers = 1;
-    }
-    else {
-        z_shift = length / layers;
-    }
+		for (int j = 0; j < layers; j++) {
+			r = xx + (espC * j);
+
+			pontoX = r + espC; //shift em x
+			pontoY = s + espA; //shift em y
+
+			//face da frente
+			coordenadas1.x = r;
+			coordenadas1.y = s;
+			coordenadas1.z = z;
+			resultado.push_back(coordenadas1);
+
+			coordenadas2.x = pontoX;
+			coordenadas2.y = s;
+			coordenadas2.z = z;
+			resultado.push_back(coordenadas2);
+
+			coordenadas3.x = pontoX;
+			coordenadas3.y = pontoY;
+			coordenadas3.z = z;
+			resultado.push_back(coordenadas3);
+			
+			coordenadas4.x = pontoX;
+			coordenadas4.y = pontoY;
+			coordenadas4.z = z;
+			resultado.push_back(coordenadas4);
+			
+			coordenadas5.x = r;
+			coordenadas5.y = pontoY;
+			coordenadas5.z = z;
+			resultado.push_back(coordenadas5);
+
+			coordenadas6.x = pontoX;
+			coordenadas6.y = s;
+			coordenadas6.z = z;
+			resultado.push_back(coordenadas6);
+			
+
+			//face de trás
+			coordenadas1.x = r;
+			coordenadas1.y = s;
+			coordenadas1.z = zz;
+			resultado.push_back(coordenadas1);
+
+			coordenadas2.x = r;
+			coordenadas2.y = pontoY;
+			coordenadas2.z = zz;
+			resultado.push_back(coordenadas2);
+			
+			coordenadas3.x = pontoX;
+			coordenadas3.y = pontoY;
+			coordenadas3.z = zz;
+			resultado.push_back(coordenadas3);
+			
+			coordenadas4.x = pontoX;
+			coordenadas4.y = pontoY;
+			coordenadas4.z = zz;
+			resultado.push_back(coordenadas4);
+			
+			coordenadas5.x = pontoX;
+			coordenadas5.y = s;
+			coordenadas5.z = zz;
+			resultado.push_back(coordenadas5);
+			
+			coordenadas6.x = r;
+			coordenadas6.y = s;
+			coordenadas6.z = zz;
+			resultado.push_back(coordenadas6);
+		}
+	}
+	
+	//face de cima e de baixo
+	//começa numa posição Z e depois faz a linha toda de X e no fim sobe uma posição de Y
+	y = height / 2;
+	yy = -y;
+	x = length / 2;
+	xx = -x;
+	z = width / 2;
+	zz = -z;
+
+	for (int i = 0; i < layers; i++) {
+		s = zz + (espL * i);
 
 
-    for (int i = 0; i < layers; i++) {
-        for (int j = 0; j < layers; j++) {
+		for (int j = 0; j < layers; j++) {
 
-            //cria a face da frente de XY
-            coordenadas1.x = -x_pos + (j*x_shift);
-            coordenadas1.y = -y_pos + (i*y_shift);
-            coordenadas1.z = length;
-            resultado.push_back(coordenadas1);
+			r = xx + (espC * j);
 
-            coordenadas2.x = (-x_pos + x_shift) + (j*x_shift);
-            coordenadas2.y = -y_pos + (i*y_shift);
-            coordenadas2.z = length;
-            resultado.push_back(coordenadas2);
+			pontoX = r + espC;
+			pontoZ = s + espL;
 
-            coordenadas3.x = (-x_pos + x_shift) + (j*x_shift);
-            coordenadas3.y = (-y_pos + y_shift) + (i*y_shift);
-            coordenadas3.z = length;
-            resultado.push_back(coordenadas3);
+			//face da cima
+			coordenadas1.x = r;
+			coordenadas1.y = y;
+			coordenadas1.z = s;
+			resultado.push_back(coordenadas1);
 
-            coordenadas4.x = -x_pos + (j*x_shift);
-            coordenadas4.y = -y_pos + (i*y_shift);
-            coordenadas4.z = length;
-            resultado.push_back(coordenadas4);
+			coordenadas2.x = r;
+			coordenadas2.y = y;
+			coordenadas2.z = pontoZ;
+			resultado.push_back(coordenadas2);
+			
+			coordenadas3.x = pontoX;
+			coordenadas3.y = y;
+			coordenadas3.z = s;
+			resultado.push_back(coordenadas3);
 
-            coordenadas5.x = (-x_pos + x_shift) + (j*x_shift);
-            coordenadas5.y = (-y_pos + y_shift) + (i*y_shift);
-            coordenadas5.z = length;
-            resultado.push_back(coordenadas5);
+			coordenadas4.x = r;
+			coordenadas4.y = y;
+			coordenadas4.z = pontoZ;
+			resultado.push_back(coordenadas4);
 
-            coordenadas6.x = -x_pos + (j*x_shift);
-            coordenadas6.y = (-y_pos + y_shift) + (i*y_shift);
-            coordenadas6.z = length;
-            resultado.push_back(coordenadas6);
-        }
-    }
+			coordenadas5.x = pontoX;
+			coordenadas5.y = y;
+			coordenadas5.z = pontoZ;
+			resultado.push_back(coordenadas5);
+			
+			coordenadas6.x = pontoX;
+			coordenadas6.y = y;
+			coordenadas6.z = s;
+			resultado.push_back(coordenadas6);
 
 
-    return resultado;
+			//face de baixo
+			coordenadas1.x = r;
+			coordenadas1.y = yy;
+			coordenadas1.z = s;
+			resultado.push_back(coordenadas1);
+
+			coordenadas2.x = pontoX;
+			coordenadas2.y = yy;
+			coordenadas2.z = s;
+			resultado.push_back(coordenadas2);
+
+			coordenadas3.x = pontoX;
+			coordenadas3.y = yy;
+			coordenadas3.z = pontoZ;
+			resultado.push_back(coordenadas3);
+			
+			coordenadas4.x = pontoX;
+			coordenadas4.y = yy;
+			coordenadas4.z = pontoZ;
+			resultado.push_back(coordenadas4);
+
+			coordenadas5.x = r;
+			coordenadas5.y = yy;
+			coordenadas5.z = pontoZ;
+			resultado.push_back(coordenadas5);
+			
+			coordenadas6.x = r;
+			coordenadas6.y = yy;
+			coordenadas6.z = s;
+			resultado.push_back(coordenadas6);
+		}
+	}
+
+	//face da esquerda e da direita
+	//começa numa posição de Z faz toda a linha e depois sobe uma unidade de Y.
+	y = height / 2;
+	yy = -y;
+	x = length / 2;
+	xx = -x;
+	z = width / 2;
+	zz = -z;
+
+	for (int i = 0; i < layers; i++) {
+		s = yy + (espA * i);
+
+		for (int j = 0; j < layers; j++) {
+			r = zz + (espL * j);
+
+			pontoZ = r + espL;
+			pontoY = s + espA;
+
+			//face da esquerda
+			coordenadas1.x = xx;
+			coordenadas1.y = s;
+			coordenadas1.z = r;
+			resultado.push_back(coordenadas1);
+
+			coordenadas2.x = xx;
+			coordenadas2.y = s;
+			coordenadas2.z = pontoZ;
+			resultado.push_back(coordenadas2);
+
+			coordenadas3.x = xx;
+			coordenadas3.y = pontoY;
+			coordenadas3.z = pontoZ;
+			resultado.push_back(coordenadas3);
+
+			coordenadas4.x = xx;
+			coordenadas4.y = pontoY;
+			coordenadas4.z = pontoZ;
+			resultado.push_back(coordenadas4);
+
+			coordenadas5.x = xx;
+			coordenadas5.y = pontoY;
+			coordenadas5.z = r;
+			resultado.push_back(coordenadas5);
+			
+			coordenadas6.x = xx;
+			coordenadas6.y = s;
+			coordenadas6.z = r;
+			resultado.push_back(coordenadas6);
+
+
+			//face da direita
+			coordenadas1.x = x;
+			coordenadas1.y = s;
+			coordenadas1.z = r;
+			resultado.push_back(coordenadas1);
+			
+			coordenadas2.x = x;
+			coordenadas2.y = pontoY;
+			coordenadas2.z = r;
+			resultado.push_back(coordenadas2);
+
+			coordenadas3.x = x;
+			coordenadas3.y = s;
+			coordenadas3.z = pontoZ;
+			resultado.push_back(coordenadas3);
+
+			coordenadas4.x = x;
+			coordenadas4.y = pontoY;
+			coordenadas4.z = r;
+			resultado.push_back(coordenadas4);
+			
+			coordenadas5.x = x;
+			coordenadas5.y = pontoY;
+			coordenadas5.z = pontoZ;
+			resultado.push_back(coordenadas5);
+
+			coordenadas6.x = x;
+			coordenadas6.y = s;
+			coordenadas6.z = pontoZ;
+			resultado.push_back(coordenadas6
+);
+		}
+	}
+
+	return resultado;
 }
